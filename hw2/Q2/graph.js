104c1a482bcc283790f03f35136004b5f41c306d
Bojun Yang <bojunyang24@gmail.com>
2021-03-04 15:13:13 -0500

    finished q2

--- hw2/Q2/graph.js
+++ hw2/Q2/graph.js
@@ -39,13 +39,34 @@ d3.dsv(",", "board_games.csv", function(d) {
         .data(links)
         .enter()
         .append("path")
-        .attr("class", function(d) { return "link " + d.type; });
-  
+        .attr("class", function(d) { return "link " + d.type; })
+        .style("stroke", function(d) {
+            if (d.value == 0) {
+                return "gray"
+            } else {
+                return "green"
+            }
+        })
+        .style("stroke-width", function(d) {
+            if (d.value == 0) {
+                return 5
+            } else {
+                return 2
+            }
+        })
+        .style("stroke-dasharray", function(d) {
+            if (d.value == 0) {
+                return null;
+            } else {
+                return "7 7"
+            }
+        });
     // define the nodes
     var node = svg.selectAll(".node")
         .data(force.nodes())
         .enter().append("g")
         .attr("class", "node")
+        .on("dblclick", release)
         .call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
@@ -53,7 +74,23 @@ d3.dsv(",", "board_games.csv", function(d) {
   
     // add the nodes
     node.append("circle")
-        .attr("r", 5);
+    .attr("r", function(d) {
+        d.weight = links.filter(function(l) {
+            return l.source.index == d.index || l.target.index == d.index
+        }).length;
+        var minRadius = 2;
+        var maxRadius = 60;
+        return Math.min(minRadius + (d.weight * 2.5), maxRadius);
+    })
+    .style("fill", nodeColor);
+    
+    // label the nodes with names
+    node.append("text")
+        // .attr("text-anchor","end")
+        .attr("dx","8px")
+        .attr("dy","-6px")
+        .text(function(d){return d.name})
+        .style("font-weight", "bold");
   
     // add the curvy lines
     function tick() {
@@ -75,27 +112,45 @@ d3.dsv(",", "board_games.csv", function(d) {
     };
   
     function dragstarted(d) {
+        console.log('dragstarted');
         if (!d3.event.active) force.alphaTarget(0.3).restart();
         d.fx = d.x;
         d.fy = d.y;
+        d3.select(this).select("circle")
+            .style("fill","purple");
     };
   
     function dragged(d) {
+        console.log('dragged');
         d.fx = d3.event.x;
         d.fy = d3.event.y;
     };
   
     function dragended(d) {
+        console.log('gradended');
         if (!d3.event.active) force.alphaTarget(0);
-        if (d.fixed == true) {
-            d.fx = d.x;
-            d.fy = d.y;
-        }
-        else {
-            d.fx = null;
-            d.fy = null;
-        }
+        d.fx = d.x;
+        d.fy = d.y;
     };
+
+    function release(d) {
+        console.log('release');
+        d.fx = null;
+        d.fy = null;
+        
+        d3.select(this).select("circle")
+            .style("fill", nodeColor);
+    }
+
+    function nodeColor(d) {
+        if (d.weight < 3) {
+            return "#e5f5f9";
+        } else if (d.weight < 6) {
+            return "#a8ddb5";
+        } else {
+            return "#43a2ca";
+        }
+    }
     
   }).catch(function(error) {
     console.log(error);
